b952990cfdf8be0b816974d8400d359d2d05f391
Shawn Nirappil <snirappil@appirio.com>
2020-09-22 16:57:41 -0500

    shows difference in rank day by day

--- analysis.py
+++ analysis.py
@@ -1,7 +1,22 @@
 import pandas as pd
 import glob
 
+def negative(x):
+	if x is not None and x != 0:
+		return x * -1
+	else:
+		return None
+
+def change(frame):
+	frame = frame.diff(axis=1)
+	for column in frame.columns:
+		frame[column] = frame[column].apply(negative)
+
+	frame = frame.sort_values(frame.columns[len(frame.columns) - 1], ascending = False)
+	return frame	
+
 all_files = glob.glob("./logs/*.csv")
+pd.options.display.float_format = '{:.1f}'.format
 
 li = []
 
@@ -12,9 +27,12 @@ for filename in all_files:
     li.append(df)
 
 try:
-	frame = pd.concat(li, axis=1, join='outer', ignore_index=False, sort=False)
-	frame['Rank'] = frame.mean(axis=1)
+	frame = pd.concat(li, axis=1, join='outer', ignore_index=False, sort=False)	
 	frame = frame.reindex(sorted(frame.columns), axis=1)
+	print('CHANGES OVER TIME')
+	print(change(frame))
+	frame['Rank'] = frame.mean(axis=1)
+	print('\nOVERALL RANK')
 	print(frame.sort_values('Rank'))
 except ValueError:
 	print("No Files Found! Get Data using retrieveSource.py")
